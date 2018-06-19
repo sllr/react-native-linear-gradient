d5ee4dc887b6e179be7b7aa76a1b9dff511a2802
John Hunter Bowen <johnhunterbowen@gmail.com>
2018-06-19 13:48:12 -0600

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,17 +2,13 @@ import * as React from "react";
 import * as ReactNative from "react-native";
 
 declare module "react-native-linear-gradient" {
-  export interface LinearGradientProps extends ReactNative.ViewProperties {
-    colors: string[];
-    start?: any;
-    end?: any;
-    locations?: number[];
-  }
 
-  export class LinearGradient extends React.Component<
-    LinearGradientProps,
-    any
-  > {}
+    interface LinearGradientProps extends ReactNative.ViewProperties {
+        colors: string[],
+        start?: any,
+        end?: any,
+        locations?: number[]
+    }
 
-  export default LinearGradient;
+    export default class LinearGradient extends React.Component<LinearGradientProps, any> { }
 }
